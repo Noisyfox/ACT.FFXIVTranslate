2b3478718a6be74ca071db29972832aa79716bd9
noisyfox <timemanager.rick@gmail.com>
2017-07-12 19:42:23 +0800

    Basic structure

---
+++ ACT.FFXIVTranslate/ACT.FFXIVTranslate/FFXIVTranslatePlugin.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Advanced_Combat_Tracker;
+
+namespace ACT.FFXIVTranslate
+{
+    public class FFXIVTranslatePlugin : IActPluginV1
+    {
+        private readonly string _settingsFile = Path.Combine(ActGlobals.oFormActMain.AppDataFolder.FullName, "Config\\ACT.FFXIVTranslate.config.xml");
+
+        private TabPage parentTabPage;
+        private Label statusLabel;
+
+        public void InitPlugin(TabPage pluginScreenSpace, Label pluginStatusText)
+        {
+            parentTabPage = pluginScreenSpace;
+            statusLabel = pluginStatusText;
+
+            // Load FFXIV plugin's assembly if needed
+            AppDomain.CurrentDomain.AssemblyResolve += (o, e) =>
+            {
+                var simpleName = new string(e.Name.TakeWhile(x => x != ',').ToArray());
+                if (simpleName == "FFXIV_ACT_Plugin")
+                {
+                    var query = ActGlobals.oFormActMain.ActPlugins.Where(x => x.lblPluginTitle.Text == "FFXIV_ACT_Plugin.dll");
+                    var plugin = query.FirstOrDefault();
+
+                    if (plugin != null)
+                    {
+                        return System.Reflection.Assembly.LoadFrom(plugin.pluginFile.FullName);
+                    }
+                }
+
+                return null;
+            };
+
+            parentTabPage.Text = "Talk Translate";
+
+            var mainControl = new FFXIVTranslateTabControl();
+            mainControl.AttachToACT(parentTabPage);
+        }
+
+        public void DeInitPlugin()
+        {
+        }
+    }
+}
---
+++ ACT.FFXIVTranslate/ACT.FFXIVTranslate/FFXIVTranslateTabControl.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ACT.FFXIVTranslate
+{
+    public partial class FFXIVTranslateTabControl : UserControl
+    {
+        public FFXIVTranslateTabControl()
+        {
+            InitializeComponent();
+        }
+
+        public void AttachToACT(TabPage parentTabPage)
+        {
+            parentTabPage.Controls.Add(this);
+            parentTabPage.Resize += ParentTabPageOnResize;
+            ParentTabPageOnResize(parentTabPage, null);
+        }
+
+        private void ParentTabPageOnResize(object sender, EventArgs eventArgs)
+        {
+            Location = new Point(0, 0);
+            Size = ((TabPage)sender).Size;
+        }
+    }
+}

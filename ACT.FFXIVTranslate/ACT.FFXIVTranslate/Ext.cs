ad46898791fa71bdf190f595f7a7bed7114571e8
noisyfox <timemanager.rick@gmail.com>
2017-09-26 15:17:38 +1000

    Remove TranslateForm

--- ACT.FFXIVTranslate/ACT.FFXIVTranslate/Ext.cs
+++ ACT.FFXIVTranslate/ACT.FFXIVTranslate/Ext.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Drawing;
-using System.Linq;
-using System.Text;
+﻿using System.Drawing;
 using System.Windows.Forms;
 using Advanced_Combat_Tracker;
 
@@ -18,31 +14,6 @@ namespace ACT.FFXIVTranslate
         public static string ToHexString(this Color c)
         {
             return "#" + c.R.ToString("X2") + c.G.ToString("X2") + c.B.ToString("X2");
-        }
-    }
-
-    public static class ThreadInvokesExt
-    {
-        private delegate void RichTextBoxAppendRtfCallback(Form parent, RichTextBox target, string rtf);
-
-        public static void RichTextBoxAppendRtf(Form parent, RichTextBox target, string rtf)
-        {
-            if (target.InvokeRequired)
-            {
-                RichTextBoxAppendRtfCallback appendTextCallback = RichTextBoxAppendRtf;
-                parent.Invoke(appendTextCallback, parent, target, rtf);
-            }
-            else
-            {
-                target.SelectionStart = target.TextLength;
-                target.SelectionLength = 0;
-
-                target.SelectedRtf = rtf;
-//                target.SelectionFont = font;
-//                target.AppendText(text);
-//                target.SelectionColor = target.ForeColor;
-                target.ScrollToCaret();
-            }
         }
     }
 }

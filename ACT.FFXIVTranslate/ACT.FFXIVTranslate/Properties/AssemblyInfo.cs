92737cc6af2c70fae05f6c51a71fc8867e381c44
noisyfox <timemanager.rick@gmail.com>
2019-11-03 18:25:40 +1100

    Bump version

--- ACT.FFXIVTranslate/ACT.FFXIVTranslate/Properties/AssemblyInfo.cs
+++ ACT.FFXIVTranslate/ACT.FFXIVTranslate/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ACT.FFXIVTranslate")]
-[assembly: AssemblyCopyright("Copyright © Noisyfox 2017")]
+[assembly: AssemblyCopyright("Copyright © Noisyfox 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,7 +32,7 @@ using System.Runtime.InteropServices;
 //可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值，
 // 方法是按如下所示使用“*”: :
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.0.0")]
-[assembly: AssemblyFileVersion("1.2.0.0")]
+[assembly: AssemblyVersion("1.3.0.0")]
+[assembly: AssemblyFileVersion("1.3.0.0")]
 
 [assembly: InternalsVisibleTo("ACT.FFXIVTranslate.Test")]
